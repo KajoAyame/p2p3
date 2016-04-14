000ce4de2f6c27e37e46976a05b625dc5bbbc344
Xinghao Zhou <zhou.xinghao.1991@gmail.com>
2016-04-13 21:30:03 -0400

    add src file

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+extern crate crust;
+extern crate time;
+#[macro_use]
+extern crate maidsafe_utilities;
+extern crate rustc_serialize;
+extern crate docopt;
+
+mod commit;
+mod compile;
+mod logger;
+pub mod network;
+mod permission;
+mod storage;
+mod ui;
+mod woot;
