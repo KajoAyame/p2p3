0d684b1d700c50b8a53d9c7e799c147b8114760e
Xinghao Zhou <zhou.xinghao.1991@gmail.com>
2016-04-25 06:40:42 -0700

    7

--- target/debug/deps/semver-02cfe1f59597f39c.d
+++
@@ -1,7 +0,0 @@
-/Users/Signal/Documents/github/p2p3/target/debug/deps/semver-02cfe1f59597f39c.d: /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/semver-0.1.20/src/lib.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/semver-0.1.20/src/version.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/semver-0.1.20/src/version_req.rs
-
-/Users/Signal/Documents/github/p2p3/target/debug/deps/libsemver-02cfe1f59597f39c.rlib: /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/semver-0.1.20/src/lib.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/semver-0.1.20/src/version.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/semver-0.1.20/src/version_req.rs
-
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/semver-0.1.20/src/lib.rs:
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/semver-0.1.20/src/version.rs:
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/semver-0.1.20/src/version_req.rs:
