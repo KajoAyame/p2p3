797561519f36be17e34ab7dbee34f78517bd4321
Xinghao Zhou <zhou.xinghao.1991@gmail.com>
2016-04-25 05:17:19 -0700

    1

--- target/debug/deps/hpack-8f91a695370f3d75.d
+++
@@ -1,8 +0,0 @@
-/Users/Signal/Documents/github/p2p3/target/debug/deps/hpack-8f91a695370f3d75.d: /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/lib.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/encoder.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/decoder.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/huffman.rs
-
-/Users/Signal/Documents/github/p2p3/target/debug/deps/libhpack-8f91a695370f3d75.rlib: /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/lib.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/encoder.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/decoder.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/huffman.rs
-
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/lib.rs:
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/encoder.rs:
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/decoder.rs:
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/hpack-0.2.0/src/huffman.rs:
--- target/debug/deps/regex_syntax-3b2362e6ea0109f1.d
+++
@@ -1,9 +0,0 @@
-/Users/Signal/Documents/github/p2p3/target/debug/deps/libregex_syntax-3b2362e6ea0109f1.rlib: /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/lib.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/literals.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/parser.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/unicode.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/properties.rs
-
-/Users/Signal/Documents/github/p2p3/target/debug/deps/regex_syntax-3b2362e6ea0109f1.d: /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/lib.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/literals.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/parser.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/unicode.rs /Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/properties.rs
-
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/lib.rs:
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/literals.rs:
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/parser.rs:
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/unicode.rs:
-/Users/Signal/.cargo/registry/src/github.com-88ac128001ac3a9a/regex-syntax-0.3.1/src/properties.rs:
